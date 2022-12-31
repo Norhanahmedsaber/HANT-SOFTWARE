8f967611c717726c461837dcfd0060fcaadd2803
2152001 <tokaabdelgawad60@gmail.com>
2022-12-31 14:38:14 +0200

    login

--- pages/signUp/signup.js
+++ pages/signUp/signup.js
@@ -1,8 +1,27 @@
+let users = [];
+const userslls=JSON.parse(localStorage.getItem("users"))
+if(userslls)
+{
+    users=userslls;
+}
 document.querySelector("#signup").addEventListener('click',()=>{
     signup()
 })
 function signup()
 {
-    const email=document.querySelector("#email-text").value
+    const email=document.querySelector("#email-Text").value
+    const password=document.querySelector("#password-Text").value
+    const firstName= document.querySelector("#firstname").value
+    const lastName=document.querySelector("#lastname").value
+    const confirmedPassword = document.querySelector("#confirm").value
+    const user = {
+        email,
+        password,
+        firstName,
+        lastName
+    }
+    users.push(user)
+    const userstr = JSON.stringify(users);
+    localStorage.setItem("users" , userstr)
 
 }
