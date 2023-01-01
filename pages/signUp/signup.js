05c6937e52fe7da334bb9d00f67134088217f8d1
hussien-mostafa <hussienmostafa888@gmail.com>
2023-01-01 12:11:02 +0200

    fr

--- pages/signUp/signup.js
+++ pages/signUp/signup.js
@@ -1,28 +1,26 @@
 let users = [];
-const userslls=JSON.parse(localStorage.getItem("users"))
-if(userslls)
-{
-    users=userslls;
+const userslls = JSON.parse(localStorage.getItem("users"));
+if (userslls) {
+  users = userslls;
 }
-document.querySelector("#signup").addEventListener('click',()=>{
-    signup()
-})
-function signup()
-{
-    const email=document.querySelector("#email-Text").value
-    const password=document.querySelector("#password-Text").value
-    const firstName= document.querySelector("#firstname").value
-    const lastName=document.querySelector("#lastname").value
-    const confirmedPassword = document.querySelector("#confirm").value
-    const user = {
-        email,
-        password,
-        firstName,
-        lastName,
-        name: firstName + " " + lastName
-    }
-    users.push(user)
-    const userstr = JSON.stringify(users);
-    localStorage.setItem("users" , userstr);
-
+document.querySelector("#signup").addEventListener("click", () => {
+  signup();
+});
+function signup() {
+  const email = document.querySelector("#email-Text").value;
+  const password = document.querySelector("#password-Text").value;
+  const firstName = document.querySelector("#firstname").value;
+  const lastName = document.querySelector("#lastname").value;
+  const confirmedPassword = document.querySelector("#confirm").value;
+  const user = {
+    email,
+    password,
+    firstName,
+    lastName,
+    name: firstName + " " + lastName,
+    id: uuid(),
+  };
+  users.push(user);
+  const userstr = JSON.stringify(users);
+  localStorage.setItem("users", userstr);
 }
