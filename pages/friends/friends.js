8f7289502728bd073025fbce14e920cf4b8151c0
habiba <habibaemad635@gmail.com>
2023-01-02 12:42:49 +0200

    search / picture

--- pages/friends/friends.js
+++ pages/friends/friends.js
@@ -1,19 +1,28 @@
 let users = [];
+
 const table = document.querySelector(".div-table");
 const loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
 const wrapperEl = document.querySelector(".wrapper");
 users = JSON.parse(localStorage.getItem("users"));
+
+let filteredUsers=users;
+
+
 function render() {
-  if (users) {
-    users.forEach((user, index) => {
+  table.innerHTML = ""
+  if (filteredUsers) {
+    filteredUsers.forEach((user, index) => {
+      if(!user.img){
+        user.img = "/pages/friends/images/585e4bf3cb11b227491c339a.png";
+      }
       if (user.id !== loggedInUser.id) {
-        const html = `<div >   
+        const html = `<div  class="sss">   
         <img class="image-1" src=${
           user.img
         } alt="personal image" title="friend name">
         <p class="name">${user.name} </p>
         <button id="no${index}" class="edit-btn" title="Show profile">
-            <a href=${`/pages/friends/friend/?id=${user.id}`}><img src="images/headhunting.png" alt="" /></a>
+            <a href=${`/pages/friends/friend/?id=${user.id}`}><img src="images/headhunting.png" alt="show profile" /></a>
         </button>
         <hr>
         </div>`;
@@ -23,3 +32,26 @@ function render() {
   }
 }
 render();
+
+const names =document.querySelector(".name");
+const search =document.querySelector(" input");
+const addForm = document.querySelector('.add');
+const div = document.querySelector('.div-table');
+
+const filterFriends = term=>{
+  filteredUsers = users.filter( user=> {
+    if (user.name.toLowerCase().includes(term)) {
+      return user;
+    }
+     });
+}
+
+search.addEventListener("input" , e=>{
+const term = search.value.trim().toLowerCase();
+filterFriends(term);
+render();
+  });
+
+
+ 
+
