996155bb8b2764ddf3ec5623b11791a00622c42b
hussien-mostafa <hussienmostafa888@gmail.com>
2023-01-02 14:45:55 +0200

    from time in post

--- pages/profile/functions.js
+++ pages/profile/functions.js
@@ -10,11 +10,15 @@ let info = {
 };
 function getNow() {
   var today = new Date();
-  var date =
-    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-  var time =
-    today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-  return date + " " + time;
+  const date2 = {
+    year: today.getFullYear(),
+    month: today.getMonth() + 1,
+    day: today.getDay() + 1,
+    hour: today.getHours() - 2,
+    min: today.getMinutes(),
+    sec: today.getSeconds(),
+  };
+  return date2;
 }
 function createPost() {
   const files = [];
@@ -25,7 +29,7 @@ function createPost() {
     id: uuid(),
     authorId: user.id,
     authorName: user.name,
-    creationData: getNow(),
+    creationData: new Date(),
     text: document.querySelector(".input2").value,
     files,
   };
@@ -95,19 +99,23 @@ function renderPost(post) {
   const labelEl = document.createElement("label");
   labelEl.textContent = post.authorName;
 
+  const labelDateEl = document.createElement("label");
+  labelDateEl.textContent = date(post.creationData);
+
   const updateEl = document.createElement("button");
   updateEl.id = "update-btn";
   const updateIcon = document.createElement("img");
-  updateIcon.setAttribute("src", "/pages/profile/Images/edit (1).png")
+  updateIcon.setAttribute("src", "/pages/profile/Images/edit (1).png");
   updateEl.appendChild(updateIcon);
 
   const deleteEl = document.createElement("button");
   deleteEl.id = "delete-btn";
   const deleteIcon = document.createElement("img");
-  deleteIcon.setAttribute("src", "/pages/profile/Images/delete (2).png")
+  deleteIcon.setAttribute("src", "/pages/profile/Images/delete (2).png");
   deleteEl.appendChild(deleteIcon);
 
   headerEl.appendChild(labelEl);
+  headerEl.appendChild(labelDateEl);
   headerEl.appendChild(updateEl);
   headerEl.appendChild(deleteEl);
 
@@ -120,21 +128,22 @@ function renderPost(post) {
   textAreaEl.setAttribute("name", "txt");
   textAreaEl.innerHTML = post.text;
   textAreaEl.style.display = "block";
-  
+
   formEl.appendChild(textAreaEl);
 
   const listEl = document.createElement("div");
   listEl.classList.add("list");
-  listEl.style.display = "block"
+  listEl.style.display = "block";
   post.files.forEach((file) => {
     const itemEl = document.createElement("li");
-    itemEl.id = file.substring(26)
+    itemEl.id = file.substring(26);
     itemEl.innerHTML = file.substring(26);
     const labelEl = document.createElement("span");
     labelEl.classList.add("cancel");
     const iEl = document.createElement("i");
     labelEl.appendChild(iEl);
     itemEl.appendChild(labelEl);
+
     const buttonEl = document.createElement("button");
     buttonEl.setAttribute("id", "download-btn");
     const iconEl = document.createElement("img");
@@ -142,9 +151,8 @@ function renderPost(post) {
     buttonEl.appendChild(iconEl);
     itemEl.appendChild(buttonEl);
     listEl.appendChild(itemEl);
-    
-    
   });
+
   postEl.appendChild(headerEl);
   postEl.appendChild(formEl);
   postEl.appendChild(listEl);
@@ -159,7 +167,7 @@ function renderUploaded() {
   uploadedEl.innerHTML = "";
   uploaded.forEach((file) => {
     const itemEl = document.createElement("li");
-    itemEl.id = file
+    itemEl.id = file;
     itemEl.innerHTML = file;
     const labelEl = document.createElement("span");
     labelEl.classList.add("cancel");
@@ -174,10 +182,7 @@ function renderUploaded() {
     itemEl.appendChild(buttonEl);
     buttonEl.addEventListener("click", () => {
       uploaded.forEach((deletedFile, index) => {
-        if (
-          deletedFile ===
-          buttonEl.parentElement.id
-        ) {
+        if (deletedFile === buttonEl.parentElement.id) {
           uploaded.splice(index, 1);
         }
       });
@@ -185,4 +190,49 @@ function renderUploaded() {
     });
     uploadedEl.appendChild(itemEl);
   });
+}
+function date(d) {
+  const date1 = {
+    year: parseInt(d.toString().substring(0, 4)),
+    month: parseInt(d.toString().substring(5, 7)),
+    day: parseInt(d.toString().substring(8, 10)),
+    hour: parseInt(d.toString().substring(11, 13)),
+    min: parseInt(d.toString().substring(14, 16)),
+    sec: parseInt(d.toString().substring(17, 19)),
+  };
+
+  const date2 = getNow();
+
+  console.log(date2 - date1);
+  if (isNaN(date2.year - date1.year)) return "just now";
+  else if (date2.year - date1.year !== 0) {
+    return date2.year - date1.year + " year ago";
+  } else if (date2.month - date1.month !== 0) {
+    return date2.month - date1.month + " month ago";
+  } else if (date2.day - date1.day !== 0) {
+    return date2.day - date1.day + " day ago";
+  } else if (date2.hour - date1.hour !== 0) {
+    return date2.hour - date1.hour + " hour ago";
+  } else if (date2.min - date1.min !== 0) {
+    return date2.min - date1.min + " min ago";
+  } else return date2.sec - date1.sec + " sec ago";
+
+  // console.log(d);
+  // console.log(date1.year);
+  // console.log(date1.month);
+  // console.log(date1.day);
+  // console.log(date1.hour);
+  // console.log(date1.min);
+  // console.log(date1.sec);
+
+  // const x = new Date() - d;
+  // if (x / 1000 < 60) return Math.floor(x / 1000) + " sec";
+  // else if (x / (1000 * 60) < 60) return Math.floor(x / (1000 * 60)) + " Min";
+  // else if (x / (1000 * 60 * 60) < 24)
+  //   return Math.floor(x / (1000 * 60 * 60)) + " Hour";
+  // else if (x / (1000 * 60 * 60 * 24) < 30)
+  //   return Math.floor(x / (1000 * 60 * 60 * 24)) + " Day";
+  // else if (x / (1000 * 60 * 60 * 24 * 30) < 12)
+  //   return Math.floor(x / (1000 * 60 * 60 * 24 * 30)) + " Month";
+  // else return Math.floor(x / (1000 * 60 * 60 * 24 * 30 * 356.25)) + " year";
 }
